9c7eedb9c5d1423199ff4c8ad558450831ec1a15
NP User <npuser@np.io>
2017-11-01 22:26:27 +0000

    done

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { LinkListComponent } from '../link-list/link-list.component';
 import { SystemListComponent } from '../system/system-list/system-list.component';
 
@@ -8,14 +8,14 @@ const routes = [
   { path: '', redirectTo: '/links', pathMatch: 'full'},
   { path: 'links', component: LinkListComponent },
   { path: 'systems', component: SystemListComponent },
-  { path: 'news', loadChildren: '../sports/sports.module#SportsModule' },
-  { path: 'news', loadChildren: '../markets/markets.module#MarketsModule'}
+  { path: 'news', loadChildren: '../sports/sports.module#SportsModule' }, // lazy loading
+  { path: 'news', loadChildren: '../markets/markets.module#MarketsModule'} // lazy loading
 ];
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [ RouterModule ],
   declarations: []
